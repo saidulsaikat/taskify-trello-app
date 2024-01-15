4d07c2950692804c1c79330663aebd52d96d280b
saidulhasan <saidulhasan562@gmail.com>
2024-01-15 19:37:32 +0600

    add Form Components

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,19 @@
+import { db } from "@/lib/db";
+import { Form } from "./form";
+import { Board } from "./board";
+
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany();
+  return (
+    <div className="flex flex-col space-y-4">
+      <Form />
+      <div className=" space-y-2">
+        {boards.map((board) => (
+          <Board key={board.id} title={board.title} id={board.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default OrganizationIdPage;
---
+++ lib/create-safe-action.ts
@@ -0,0 +1,28 @@
+import { date, z } from "zod";
+
+export type FieldErrors<T> = {
+  [K in keyof T]?: string[];
+};
+
+export type ActionState<TInput, TOutput> = {
+  fieldErrors?: FieldErrors<TInput>;
+  error?: string | null;
+  data?: TOutput;
+};
+
+export const createSafeAction = <TInput, TOutput>(
+  schema: z.Schema<TInput>,
+  handler: (validationData: TInput) => Promise<ActionState<TInput, TOutput>>
+) => {
+  return async (date: TInput): Promise<ActionState<TInput, TOutput>> => {
+    const validationResult = schema.safeParse(date);
+    if (!validationResult.success) {
+      return {
+        fieldErrors: validationResult.error.flatten()
+          .fieldErrors as FieldErrors<TInput>,
+      };
+    }
+
+    return handler(validationResult.data);
+  };
+};
