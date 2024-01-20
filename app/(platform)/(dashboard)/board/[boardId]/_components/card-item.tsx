1221741a49e2ada44b941d267dd2d9c829be890d
saidulhasan <saidulhasan562@gmail.com>
2024-01-20 21:14:31 +0600

    Little Fix

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -9,7 +9,10 @@ interface CardItemProps {
 
 export const CardItem = ({ data, index }: CardItemProps) => {
   return (
-    <div className=" truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm">
+    <div
+      role="button"
+      className=" truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
+    >
       {data.title}
     </div>
   );
