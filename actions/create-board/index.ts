fd9e0232d80b7dc06e00ba0a27815d680f9e5433
saidulhasan <saidulhasan562@gmail.com>
2024-01-24 01:38:27 +0600

    add Stripe & Board limits

--- actions/create-board/index.ts
+++ actions/create-board/index.ts
@@ -8,6 +8,8 @@ import { createSafeAction } from "@/lib/create-safe-action";
 import { CreateBoard } from "./schema";
 import { createAuditLog } from "@/lib/create-audit-log";
 import { ACTION, ENTITY_TYPE } from "@prisma/client";
+import { incrementAvailableCount, hasAvailableCount } from "@/lib/org-limit";
+import { checkSubscription } from "@/lib/subscription";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
@@ -16,6 +18,15 @@ const handler = async (data: InputType): Promise<ReturnType> => {
     return {
       error: "Unauthorized",
     };
+  }
+
+  const canCreate = await hasAvailableCount();
+  const isPro = await checkSubscription()
+
+  if (!canCreate && !isPro) {
+    return {
+      error: "You have reached your limit of free boards. Please upgrade to create more."
+    }
   }
 
   const { title, image } = data;
@@ -49,6 +60,10 @@ const handler = async (data: InputType): Promise<ReturnType> => {
         imageLinkHTML,
       },
     });
+
+    if (!isPro) {
+      await incrementAvailableCount()
+    }
 
     await createAuditLog({
       entityTitle: board.title,
